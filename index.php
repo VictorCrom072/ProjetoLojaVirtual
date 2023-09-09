066dd4d0239738a6aacb8e38e8a8ff95f6cb57ff
VictorCrom072 <portovictor072@gmail.com>
2023-09-09 20:45:55 -0300

    CromStore-Aula05

--- index.php
+++ index.php
@@ -7,8 +7,12 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <style type="text/css">
+        .navbar{ margin-bottom: 0%; } 
+    </style>
 </head>
 <body>
     <?php include 'nav.php' ?>
+    <?php include 'cabecalho.html' ?>
 </body>
 </html>
--- nav.php
+++ nav.php
@@ -1,54 +1,42 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Minha Loja</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
-<body>
-    <nav class="navbar navbar-light" style="background-color: #541DF2;">
-    <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="#">Crom Store</a>
+
+<nav class="navbar navbar-light" style="background-color: #541DF2;">
+<div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="#">Crom Store</a>
+    </div>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <ul class="nav navbar-nav">
+        <li><a href="#">Home<span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Lançamentos</a></li>
+        <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Categorias<span class="caret"></span></a>
+        <ul class="dropdown-menu">
+            <li><a href="#">Play Station</a></li>
+            <li><a href="#">Xbox</a></li>
+            <li><a href="#">Switch</a></li>
+            <li><a href="#">PC</a></li>
+            <li><a href="#">Mobile</a></li>
+        </ul>
+        </li>
+    </ul>
+    <form class="navbar-form navbar-left" role="search">
+        <div class="form-group">
+        <input type="text" class="form-control" placeholder="Pesquisar">
         </div>
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <ul class="nav navbar-nav">
-            <li><a href="#">Home<span class="sr-only">(current)</span></a></li>
-            <li><a href="#">Lançamentos</a></li>
-            <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Categorias<span class="caret"></span></a>
-            <ul class="dropdown-menu">
-                <li><a href="#">Play Station</a></li>
-                <li><a href="#">Xbox</a></li>
-                <li><a href="#">Switch</a></li>
-                <li><a href="#">PC</a></li>
-                <li><a href="#">Mobile</a></li>
-            </ul>
-            </li>
-        </ul>
-        <form class="navbar-form navbar-left" role="search">
-            <div class="form-group">
-            <input type="text" class="form-control" placeholder="Pesquisar">
-            </div>
-            <button type="submit" class="btn btn-default">Pesquisar</button>
-        </form>
-        <ul class="nav navbar-nav navbar-right">
-            <li><a href="#">Contato</a></li>
-            <li><a href="#"><span class="glyphicon glyphicon-log-in"> Logon</span></a></li>
-        </ul>
-        </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
-    </nav>
-</body>
-</html>
+        <button type="submit" class="btn btn-default">Pesquisar</button>
+    </form>
+    <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Contato</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"> Logon</span></a></li>
+    </ul>
+    </div><!-- /.navbar-collapse -->
+</div><!-- /.container-fluid -->
+</nav>
