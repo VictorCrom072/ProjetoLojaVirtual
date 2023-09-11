8d7ac30c03aa363f20b40696db754a936cfb82f1
VictorCrom072 <portovictor072@gmail.com>
2023-09-10 23:25:45 -0300

    CromStore-Aula16

--- conexao.php
+++ conexao.php
@@ -3,5 +3,5 @@
     $usuario = "Crom";
     $banco = "db_LojaVirtual";
     $senha = "12345678";
-    $cn = new PDO("mysql:host=$servidor;db_name=$banco", $usuario, $senha);
+    $cn = new PDO("mysql:host=$servidor;dbname=$banco", $usuario, $senha);
 ?>
---
+++ teste.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mostrar Produtos</title>
+</head>
+<body>
+    <?php
+        include 'conexao.php';
+        $consulta = $cn->query('select * from vw_livro');
+        $exibe = $consulta->fetch(PDO::FETCH_ASSOC);
+        echo $exibe['nm_livro'].'<br>';
+        echo $exibe['vl_preco'].'<br>';
+        echo $exibe['ds_categoria'].'<br>';
+    ?>
+</body>
+</html>
